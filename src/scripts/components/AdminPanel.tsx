78f3a6aae8eb51fc6fcca927ebd4aeab2664ba1f
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2019-03-25 18:36:15 +0300

    #3 delete work

--- src/scripts/components/AdminPanel.tsx
+++ src/scripts/components/AdminPanel.tsx
@@ -3,37 +3,44 @@ import City from "../models/city";
 import CityForm from "./AdminPanel/CityForm";
 import CityList from "./AdminPanel/CityList";
 
-export default class AdminPanel extends React.Component<{}, {}> {
-    private cities: City[];
+interface IState {
+    cities: City[];
+}
 
+export default class AdminPanel extends React.Component<{}, IState> {
     constructor(props: {}) {
         super(props);
 
-        this.cities = [
-            {
-                lat: 1,
-                lng: 2,
-                name: "Paris",
-            },
-            {
-                lat: 1,
-                lng: 2,
-                name: "Paris",
-            },
-        ];
+        this.state = {
+            cities: [
+                {
+                    lat: 1,
+                    lng: 2,
+                    name: "Paris",
+                },
+                {
+                    lat: 3,
+                    lng: 4,
+                    name: "Berlin",
+                },
+            ],
+        };
     }
 
     public render() {
         return (
             <main>
                 <CityForm />
-                <CityList cities={this.cities} onDelete={this.handleDelete} />
+                <CityList cities={this.state.cities} onDelete={this.handleDelete} />
             </main>
         );
     }
 
-    private handleDelete(city: City): void {
-        // tslint:disable-next-line:no-console
-        console.log(city.name + " deleted");
+    private handleDelete = (city: City) => {
+        const index = this.state.cities.indexOf(city);
+        this.state.cities.splice(index, 1);
+        this.setState({
+            cities: this.state.cities.slice(),
+        });
     }
 }
