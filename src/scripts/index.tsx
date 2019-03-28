31cf5de76bf44d30f157018e90293cfb7ac89214
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2019-03-28 12:11:29 +0300

    #24 created list, put it into localstorage

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
+import { seed } from "./map/seed";
 
 const root = document.getElementById("root");
 
 if (root) {
     render(<App />, root);
+    seed();
 }
