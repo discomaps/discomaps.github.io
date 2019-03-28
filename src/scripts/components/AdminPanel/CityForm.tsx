9bb9de4fbb3287772f73ed81becfd2dd74ebd802
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2019-03-28 19:31:09 +0300

    #3 dev progess

--- src/scripts/components/AdminPanel/CityForm.tsx
+++ src/scripts/components/AdminPanel/CityForm.tsx
@@ -1,12 +1,26 @@
 import * as React from "react";
 import City from "../../models/city";
 
+interface IState {
+    lat: string;
+}
+
 export interface ICityListProps {
     city: City;
     onCancel: () => any;
 }
 
-export default class CityForm extends React.Component<ICityListProps, {}> {
+export default class CityForm extends React.Component<ICityListProps, IState> {
+    private lngRef = React.createRef<HTMLInputElement>();
+
+    constructor(props: ICityListProps) {
+        super(props);
+
+        this.state = {
+            lat: "",
+        };
+    }
+
     public render() {
         const { city, onCancel } = this.props;
 
@@ -19,19 +33,30 @@ export default class CityForm extends React.Component<ICityListProps, {}> {
                 <div className="form-group">
                     <label htmlFor="name">City name</label>
                     <input type="text" className="form-control" id="name" placeholder="Enter name" />
-                    <small className="form-text text-muted">
-                        Put name of city
-                    </small>
+                    <small className="form-text text-muted">Put name of city</small>
                 </div>
                 <div className="form-group">
                     <label htmlFor="lat">Lat</label>
-                    <input type="number" className="form-control" id="lat" placeholder="lat coordinate" />
+                    <input
+                        type="number"
+                        className="form-control"
+                        id="lat"
+                        placeholder="lat coordinate"
+                        value={this.state.lat}
+                        onChange={this.handleLatChange}
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="lng">Lng</label>
-                    <input type="number" className="form-control" id="lng" placeholder="lng coordinate" />
+                    <input
+                        type="number"
+                        className="form-control"
+                        id="lng"
+                        placeholder="lng coordinate"
+                        ref={this.lngRef}
+                    />
                 </div>
-                <button type="button" className="btn btn-primary">
+                <button type="button" className="btn btn-primary" onClick={this.handleAdd}>
                     Add
                 </button>
                 <button type="button" className="btn btn-danger ml-4" onClick={onCancel}>
@@ -39,5 +64,16 @@ export default class CityForm extends React.Component<ICityListProps, {}> {
                 </button>
             </form>
         );
+    }
+
+    private handleAdd = () => {
+        alert(this.state.lat);
+        alert(this.lngRef.current.value);
+    }
+
+    private handleLatChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            lat: event.target.value,
+        });
     }
 }
